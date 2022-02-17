5922964d9a1bad29cc7278dd37c81229cc4c6b53
Kartofi <87278732+Kartofi@users.noreply.github.com>
2022-02-17 15:30:10 +0200

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,17 +1,16 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
-const kartof = require("kartof");
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const { MessageEmbed, Client, Intents, Collection } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const kartof = require('kartof');
 
-client.on('messageCreate', async message => {
-  if (message.content == "calculator") {
-    kartof.calculator({
-      message: message,
-       expire_time: 20,
+client.on('message', async message => { 
+   if (message.content == "calculator") {
+     kartof.calculator({
+       message: message, 
+       expire_time:20 , 
        channelID: message.channel.id
-    });
+     });
+   }
+   
 });
 
-client.login('token');
+client.login('Your Token');
